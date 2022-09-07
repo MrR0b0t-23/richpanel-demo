4d5260b0e161b9bef8942f4e8a2fdf6326e8248f
Ashwin <64373113+MrR0b0t-23@users.noreply.github.com>
2022-09-07 23:35:42 +0530

    Update main.go

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 
 func CORSConfig() cors.Config {
     corsConfig := cors.DefaultConfig()
-    corsConfig.AllowOrigins = []string{"https://richpanel-react.herokuapp.com"}
+    corsConfig.AllowOrigins = []string{"*"}
     corsConfig.AllowCredentials = true
     corsConfig.AddAllowHeaders("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers", "Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization")
     corsConfig.AddAllowMethods("GET", "POST", "PUT", "DELETE")
